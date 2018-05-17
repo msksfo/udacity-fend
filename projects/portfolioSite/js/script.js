d0eeaa63226b39cb1e5851cd693ee3e26c752402
Tia Esguerra <msksfo@gmail.com>
2018-05-17 13:15:28 -0700

    Add comments

--- projects/portfolioSite/js/script.js
+++ projects/portfolioSite/js/script.js
@@ -3,13 +3,14 @@ const descriptionModals = document.querySelectorAll('.description-modal');
 const closeModals = document.querySelectorAll('.close-modal');
 
 
-
+// show the modals
 for (let i = 0; i < descriptions.length; i++){
     descriptions[i].addEventListener('click', function(){
         descriptionModals[i].style.display = 'flex';
     });
 }
 
+// hide the modals
 for (let i = 0; i < closeModals.length; i++){
     closeModals[i].addEventListener('click', function(e){
         descriptionModals[i].style.display = 'none';
