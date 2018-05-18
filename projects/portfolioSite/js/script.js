24f918320440b42744201022637c3d72c0d3daad
Tia Esguerra <msksfo@gmail.com>
2018-05-18 10:14:17 -0700

    Make named functions to be passed into the event listeners

--- projects/portfolioSite/js/script.js
+++ projects/portfolioSite/js/script.js
@@ -1,30 +1,64 @@
 (function(){
     'use strict';
 
+    /* ========== Global Variables =========== */
+
     const descriptions = document.querySelectorAll('.description');
     const descriptionModals = document.querySelectorAll('.description-modal');
     const closeModals = document.querySelectorAll('.close-modal');
     const descriptionText = document.querySelectorAll('.description-text');
 
 
-    // show the modals
-    for (let i = 0; i < descriptions.length; i++){
-        descriptions[i].addEventListener('click', function(){
-            descriptionModals[i].classList.add('show-modal');
+    /* =========== Functions ============ */ 
 
-            // wait for the show modal transition to complete before showing text
-            closeModals[i].style.opacity = 1;
-            descriptionText[i].style.opacity = 1;
-        });
+    function hideModal(xModal, descriptionTxt, modal){
+        // hide the modal, the text, and the 'x' that closes the modal
+       xModal.style.opacity = 0;
+       descriptionTxt.style.opacity = 0;
+       modal.classList.remove('show-modal');
     }
 
+    function showModal(xModal, descriptionTxt, modal){
+        // show the modal, the text, and the 'x' that closes the modal
+        modal.classList.add('show-modal');
+        xModal.style.opacity = 1;
+        descriptionTxt.style.opacity = 1;
+    }
+
+
+    /* ============== Event Listeners ============ */
+
+    // show the modals on click
+    for (let i = 0; i < descriptions.length; i++){
+        /* use the bind method so the showModal function will not be immediately invoked */
+        descriptions[i].addEventListener('click', showModal.bind(this, closeModals[i], descriptionText[i], descriptionModals[i]));
+    }
+
+    // show the modals if 'enter' is pressed
+    for (let i = 0; i < descriptions.length; i++){
+        /* use the bind method so the showModal function will not be immediately invoked */
+        descriptions[i].addEventListener('keypress', showModal.bind(this, closeModals[i], descriptionText[i], descriptionModals[i]));
+    }
+/*
+    // show the modals on keypress
+    for (let i = 0; i < descriptions.length; i++){
+        descriptions[i].addEventListener('keypress', function(e){
+            if (e.keyCode === 13){
+                descriptionModals[i].classList.add('show-modal');
+
+                // wait for the show modal transition to complete before showing text
+                closeModals[i].style.opacity = 1;
+                descriptionText[i].style.opacity = 1;
+            }
+        });
+    }
+*/
     // hide the modals
     for (let i = 0; i < closeModals.length; i++){
-        closeModals[i].addEventListener('click', function(e){
-            closeModals[i].style.opacity = 0;
-            descriptionText[i].style.opacity = 0;
-            descriptionModals[i].classList.remove('show-modal');
-        });
+        /* use the bind method so the hideModal function will not be immediately invoked */
+        closeModals[i].addEventListener('click', hideModal.bind(this, closeModals[i], descriptionText[i], descriptionModals[i]));
     }
 
+
 })();
+
