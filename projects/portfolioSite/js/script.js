bdc1c2274a989df695a096d5d29d7bbae8213225
Tia Esguerra <msksfo@gmail.com>
2018-05-17 17:04:38 -0700

    Add footer with contact info

--- projects/portfolioSite/js/script.js
+++ projects/portfolioSite/js/script.js
@@ -1,25 +1,30 @@
-const descriptions = document.querySelectorAll('.description');
-const descriptionModals = document.querySelectorAll('.description-modal');
-const closeModals = document.querySelectorAll('.close-modal');
-const descriptionText = document.querySelectorAll('.description-text');
+(function(){
+    'use strict';
+
+    const descriptions = document.querySelectorAll('.description');
+    const descriptionModals = document.querySelectorAll('.description-modal');
+    const closeModals = document.querySelectorAll('.close-modal');
+    const descriptionText = document.querySelectorAll('.description-text');
 
 
-// show the modals
-for (let i = 0; i < descriptions.length; i++){
-    descriptions[i].addEventListener('click', function(){
-        descriptionModals[i].classList.add('show-modal');
+    // show the modals
+    for (let i = 0; i < descriptions.length; i++){
+        descriptions[i].addEventListener('click', function(){
+            descriptionModals[i].classList.add('show-modal');
 
-        // wait for the show modal transition to complete before showing text
-        closeModals[i].style.opacity = 1;
-        descriptionText[i].style.opacity = 1;
-    });
-}
+            // wait for the show modal transition to complete before showing text
+            closeModals[i].style.opacity = 1;
+            descriptionText[i].style.opacity = 1;
+        });
+    }
 
-// hide the modals
-for (let i = 0; i < closeModals.length; i++){
-    closeModals[i].addEventListener('click', function(e){
-        closeModals[i].style.opacity = 0;
-        descriptionText[i].style.opacity = 0;
-        descriptionModals[i].classList.remove('show-modal');
-    });
-}
+    // hide the modals
+    for (let i = 0; i < closeModals.length; i++){
+        closeModals[i].addEventListener('click', function(e){
+            closeModals[i].style.opacity = 0;
+            descriptionText[i].style.opacity = 0;
+            descriptionModals[i].classList.remove('show-modal');
+        });
+    }
+
+})();
