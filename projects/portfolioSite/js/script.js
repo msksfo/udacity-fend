92a4e2553067531bc2763811568b17c37bd655a7
Tia Esguerra <msksfo@gmail.com>
2018-05-19 07:47:54 -0700

    Delete mouseenter/mouseleave. Use click instead.

--- projects/portfolioSite/js/script.js
+++ projects/portfolioSite/js/script.js
@@ -16,7 +16,7 @@
 
     /* ================ Functions ============== */ 
 
-    function hideModal(xModal, descriptionTxt, modal){
+    function hideModal(evt, xModal, descriptionTxt, modal){
         // hide the modal, the text, and the 'x' that closes the modal
         descriptionTxt.style.opacity = 0;
         xModal.style.opacity = 0;
@@ -25,50 +25,51 @@
        // make the modal invisible to screen readers
        modal.setAttribute('aria-hidden', 'true');
 
-        // return the focus to the element that held focus before modal was open
-        lastFocusedElement.focus();
+       if (evt.type === 'keydown'){
+            // return the focus to the element that held focus before modal was open
+            lastFocusedElement.focus();
+       }
+        
     }
 
     function showModal(evt, xModal, descriptionTxt, modal){
         // show the modal, and the project description text
         modal.classList.add('show-modal');
         descriptionTxt.style.opacity = 1;
+        xModal.style.opacity = 1;
         
-        // don't show the 'x' to close the modal if the event was mouseenter
-        if (evt.type === 'mouseenter'){
-            xModal.style.opacity = 0;
-        } else {
+        if (evt.type === 'keydown'){
+            // remember which element was focused before opening modal
+            lastFocusedElement = document.activeElement;
+
+            // set focus on the close button when modal is opened
+            xModal.focus();  
+
             // keyboard trap to prevent tabbing outside the modal
             modal.addEventListener('keydown', function(e){
                 if (e.keyCode === 9){
                     e.preventDefault();
                 }
             });
-            xModal.style.opacity = 1;
         }
-
-        // remember which element was focused before opening modal
-        lastFocusedElement = document.activeElement;
 
         // make the modal visible to screen readers
         modal.setAttribute('aria-hidden', 'false');
-
-        // set focus on the close button when modal is opened
-        xModal.focus();  
     }
 
     //just for testing purposes
-    document.body.addEventListener('keydown', function(){
+    document.body.addEventListener('keydown', function(e){
         console.log(document.activeElement);
+        console.log(e.keyCode);
     })
     
 
 
     /* ============== Event Listeners ============ */
 
-    // show the modals on mouseenter and keydown
+    // show the modals on click and keydown
     for (let i = 0; i < descriptions.length; i++){
-        descriptions[i].addEventListener('mouseenter', function(e){
+        descriptions[i].addEventListener('click', function(e){
            showModal(e, closeModals[i], descriptionText[i], descriptionModals[i]);
         });
 
@@ -82,15 +83,17 @@
    
 
 
-    // hide the modals on mouseleave, and keydown
+    // hide the modals on click, and keydown
     for (let i = 0; i < descriptions.length; i++){
-        /* use the bind method so the hideModal function will not be immediately invoked  */
-        descriptionModals[i].addEventListener('mouseleave', hideModal.bind(this, closeModals[i], descriptionText[i], descriptionModals[i]));
+
+        descriptionModals[i].addEventListener('click', function(e){
+            hideModal(e, closeModals[i], descriptionText[i], descriptionModals[i]);
+        });
 
         descriptionModals[i].addEventListener('keydown', function(e){
             // close the modal if user pressed enter or esc
             if (e.keyCode === 13 || e.keyCode === 27){
-                hideModal(closeModals[i], descriptionText[i], descriptionModals[i]);
+                hideModal(e, closeModals[i], descriptionText[i], descriptionModals[i]);
             }
         });
     }
