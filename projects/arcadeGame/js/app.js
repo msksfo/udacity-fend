b06acab576571efd2b66fbaef421e09f8a1ba51a
Tia Esguerra <msksfo@gmail.com>
2018-05-27 16:21:28 -0700

    Continue working on arcade game

--- projects/arcadeGame/js/app.js
+++ projects/arcadeGame/js/app.js
@@ -4,23 +4,31 @@ var Enemy = function(x, y) {
     this.randomSpeed = Math.random() + .5;
     this.x = x;
     this.y = y;
-    this.height = 171;
-    this.width = 101;
+   
     this.sprite = 'images/char-boy.png';
+    this.height = 50;
+    this.width = 50;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     /* if the enemy's x position is off the board, reset the position to the starting position */
-
     if (this.x >= 500){
        this.x = -100;
     } else {
         (this.x += this.randomSpeed) * dt;
-        this.checkCollisions();
     }
 
+    // if any part of the two sprites is overlapping, they have crashed.
+    if ( (this.x + this.width >= player.x) && (this.y + this.height >= player.y) && (player.x + player.width >= this.x) && (player.y + player.height >= this.y) ) {
+
+        // reset player values when sprites crash
+        setTimeout(function(){
+            player.y = 402;;
+            player.x = 200;
+        }, 150);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,77 +36,149 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
-var Player = function(){
-    /*  get a random number between 0 and (board width - sprite width)
-    this random number will be the x coordinate anytime the sprite is reset  */
-    const randomX = Math.floor(Math.random() * 404 + 1);
-   
-    this.x = randomX;
-    this.y = 400;
+var Player = function(x, y){
+    this.x = x;
+    this.y = y;
+    
     this.sprite = 'images/char-pink-girl.png';
-    this.height = 171;
-    this.width = 101;
-   
+    this.height = 50;
+    this.width = 50;
 }
 
-Player.prototype.update = function(dt){
+Player.prototype.getRandomX = function(){
+     /*  get a random number between 0 and (board width - sprite width)
+    this random number will be the x coordinate anytime the sprite is reset  */
+    return Math.floor(Math.random() * 404 + 1);
+}
+
+Player.prototype.update = function(dt){}
+
+
+
+let points = document.querySelector('.points-value');
+let gameLevel = 1;
+
+function updatePoints(){
+    // add 50 points each time the player makes it to the water
+    points.innerHTML  = (points.innerHTML * 1) + 50;
+   
+    if (points.innerHTML == 50){
+        setTimeout(function(){
+            allEnemies.push(enemy2);
+        }, 800);
+    } else if (points.innerHTML == 100){
+        setTimeout(function(){
+            allEnemies.push(enemy3);
+        }, 800);
+    } 
+}
+
+function increaseLevel(){
+    let level = document.querySelector('.level-value');
+    /* set the level to the global gameLevel variable. NOTE: updateModal function increments this variable */
+    level.innerHTML = gameLevel;
+}
+
+function updateModal(){
+    // increment the global gameLevel variable, and set the level in the modal to that value
+    gameLevel += 1;
+    let modalLevel = document.querySelector('.modal-level-value');
+    modalLevel.innerHTML = gameLevel;
+}
+
+function winLevel(){
+    /* 1. update the level in the modal before showing the modal
+       2. show the modal
+       3. while the modal is hiding the score panel, update values in the score panel
+       4. hide the modal
+       5. move the player to starting position
+    */
+    updateModal();
+    const congratulations = document.querySelector('.congratulations');
+    player.x = player.x;
+    allEnemies.length = 0;
+
+    setTimeout(() => congratulations.classList.toggle('show-congratulations'), 1000);
+
+    setTimeout(function(){
+        points.innerHTML = 0;
+        increaseLevel();
+    }, 1500);
   
+    setTimeout(function(){
+        congratulations.classList.toggle('show-congratulations');
+        player.x = 200;
+        player.y = 402;
+
+    }, 3000);
+
 }
 
-Player.prototype.checkForWin = function(yPos){
-    if (yPos === -10){
+/* ============ control player movement ============== */
 
-       setTimeout(function(){
-        this.y = 400; 
-        this.x = Math.floor(Math.random() * 404 + 1);
-       }.bind(player), 1000);
+/* these four methods control player up/down/left/right movement. -created for easier readability inside the handleInput method */
+
+Player.prototype.keyUp = function (){
+    const upperLimit = -13;
+    if (this.y -83 < upperLimit){
+        this.y = upperLimit;
+    } else if (this.y - 83 === upperLimit){
+        this.y -= 83;
+        updatePoints();
+        if (points.innerHTML < 150){
+            setTimeout(function(){
+                this.y = 402; 
+                this.x = this.getRandomX();
+               }.bind(player), 800);
+        } else {
+            winLevel();
+        } 
+    } else {
+        this.y -= 83;
     }
 }
 
-Enemy.prototype.checkCollisions = function(){
-   
-    if ( (this.x + this.width >= player.x) && (this.y + this.height >= player.y) && (player.x + player.width >= this.x) && (player.y + player.height >= this.y) ) {
-
-    console.log('you crashed');
+Player.prototype.keyDown = function(){
+    const lowerLimit = 430;
+    if ((this.y + 83) > lowerLimit){
+        this.y = lowerLimit;
+    } else {
+        this.y += 83;
     }
 }
+
+Player.prototype.keyLeft = function(){
+    const leftLimit = -6;
+    if ((this.x - 101) < leftLimit){
+        this.x = leftLimit;
+    } else {
+        this.x -= 101;
+    }
+}
+
+Player.prototype.keyRight = function(){
+    const rightLimit = 410;
+    if ((this.x + 101) > rightLimit){
+        this.x = rightLimit;
+    } else {
+        this.x += 101;
+    }
+}
+
 
 Player.prototype.handleInput = function(key){
-    // the four boundaries of the gameboard
-    const upperLimit = -10;
-    const leftLimit = -6;
-    const lowerLimit = 430;
-    const rightLimit = 410;
-
     // if direction is up/down, adjust the y value by row height.
     // if direction is left/right, adjust the x value by column width.
-
    if (key === 'up'){
-        if ((this.y - 83) < upperLimit){
-            this.y = upperLimit;
-            player.checkForWin(upperLimit);
-        } else {
-            this.y -= 83;
-        }
+       player.keyUp();
    }else if (key === 'right'){
-        if ((this.x + 101) > rightLimit){
-            this.x = rightLimit;
-        } else {
-            this.x += 101;
-        }
+       player.keyRight();
    } else if (key === 'down'){
-        if ((this.y + 83) > lowerLimit){
-            this.y = lowerLimit;
-        } else {
-            this.y += 83;
-        }
+        player.keyDown();
    } else if(key === 'left'){
-        if ((this.x - 101) < leftLimit){
-            this.x = leftLimit;
-        } else {
-            this.x -= 101;
-        }
+        player.keyLeft();
    }
 }   
 
@@ -106,20 +186,16 @@ Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-const enemy1 = new Enemy(100, 70);
-//const enemy2 = new Enemy(-100, 153);
-//const enemy3 = new Enemy(-100, 236);
-
+// instantiate player and enemy objects
+const enemy1 = new Enemy(-90, 70);
+const enemy2 = new Enemy(-90, 153);
+const enemy3 = new Enemy(-90, 236);
 const allEnemies = [enemy1];
-const player = new Player();
+const player = new Player(200, 402);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method. 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
