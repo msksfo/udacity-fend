6775114479c2b33517bc6826be2868c0b8005918
Tia Esguerra <msksfo@gmail.com>
2018-05-28 21:36:08 -0700

    Delete unused photos. Add new photos. Finish game logic

--- projects/arcadeGame/js/app.js
+++ projects/arcadeGame/js/app.js
@@ -1,34 +1,51 @@
+/* ========================= Enemy Class and Methods ========================== */
+
 // Enemies our player must avoid
-var Enemy = function(x, y) {
-    // give each enemy starting coordinates and a random speed
-    this.randomSpeed = Math.random() + .5;
+var Enemy = function(x, y, sprite, height, width, direction) {
+    // give each enemy starting coordinates, a random speed, height/width, and assign sprite
     this.x = x;
     this.y = y;
-   
-    this.sprite = 'images/char-boy.png';
-    this.height = 50;
-    this.width = 50;
+    this.height = height;
+    this.width = width;
+    this.sprite = sprite;
+    this.direction = direction;
+
+    // if the sprite is a tiefighter, it should go faster
+    if (this.sprite == 'images/tiefighter.png'){
+        this.randomSpeed = Math.random() + 2;
+    } else {
+        this.randomSpeed = Math.random() + .7;
+    }
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     /* if the enemy's x position is off the board, reset the position to the starting position */
-    if (this.x >= 500){
-       this.x = -100;
-    } else {
-        (this.x += this.randomSpeed) * dt;
-    }
 
+    if (this.direction === 'rl'){
+        if (this.x <= -100){
+            this.x = 500;
+        } else {
+            (this.x -= this.randomSpeed) * dt;
+        }
+    } else {
+        if (this.x >= 500){
+            this.x = -100;
+         } else {
+             (this.x += this.randomSpeed) * dt;
+         }
+    }
+    
     // if any part of the two sprites is overlapping, they have crashed.
     if ( (this.x + this.width >= player.x) && (this.y + this.height >= player.y) && (player.x + player.width >= this.x) && (player.y + player.height >= this.y) ) {
-
-        // reset player values when sprites crash
+        // reset player coordinates when sprites crash. reset points to 0
         setTimeout(function(){
             player.y = 402;;
             player.x = 200;
+            points.innerHTML = 0;
         }, 150);
-    }
+    } 
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,14 +53,14 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/* ============================= Player Class and Methods ========================== */
 
-// Now write your own player class
+// player class
 var Player = function(x, y){
     this.x = x;
-    this.y = y;
-    
-    this.sprite = 'images/char-pink-girl.png';
-    this.height = 50;
+    this.y = y;  
+    this.sprite = 'images/rocket.png'
+    this.height = 35;
     this.width = 50;
 }
 
@@ -55,68 +72,7 @@ Player.prototype.getRandomX = function(){
 
 Player.prototype.update = function(dt){}
 
-
-
-let points = document.querySelector('.points-value');
-let gameLevel = 1;
-
-function updatePoints(){
-    // add 50 points each time the player makes it to the water
-    points.innerHTML  = (points.innerHTML * 1) + 50;
-   
-    if (points.innerHTML == 50){
-        setTimeout(function(){
-            allEnemies.push(enemy2);
-        }, 800);
-    } else if (points.innerHTML == 100){
-        setTimeout(function(){
-            allEnemies.push(enemy3);
-        }, 800);
-    } 
-}
-
-function increaseLevel(){
-    let level = document.querySelector('.level-value');
-    /* set the level to the global gameLevel variable. NOTE: updateModal function increments this variable */
-    level.innerHTML = gameLevel;
-}
-
-function updateModal(){
-    // increment the global gameLevel variable, and set the level in the modal to that value
-    gameLevel += 1;
-    let modalLevel = document.querySelector('.modal-level-value');
-    modalLevel.innerHTML = gameLevel;
-}
-
-function winLevel(){
-    /* 1. update the level in the modal before showing the modal
-       2. show the modal
-       3. while the modal is hiding the score panel, update values in the score panel
-       4. hide the modal
-       5. move the player to starting position
-    */
-    updateModal();
-    const congratulations = document.querySelector('.congratulations');
-    player.x = player.x;
-    allEnemies.length = 0;
-
-    setTimeout(() => congratulations.classList.toggle('show-congratulations'), 1000);
-
-    setTimeout(function(){
-        points.innerHTML = 0;
-        increaseLevel();
-    }, 1500);
-  
-    setTimeout(function(){
-        congratulations.classList.toggle('show-congratulations');
-        player.x = 200;
-        player.y = 402;
-
-    }, 3000);
-
-}
-
-/* ============ control player movement ============== */
+/* ====== control player movement ===== */
 
 /* these four methods control player up/down/left/right movement. -created for easier readability inside the handleInput method */
 
@@ -139,6 +95,7 @@ Player.prototype.keyUp = function (){
         this.y -= 83;
     }
 }
+
 
 Player.prototype.keyDown = function(){
     const lowerLimit = 430;
@@ -167,7 +124,6 @@ Player.prototype.keyRight = function(){
     }
 }
 
-
 Player.prototype.handleInput = function(key){
     // if direction is up/down, adjust the y value by row height.
     // if direction is left/right, adjust the x value by column width.
@@ -186,13 +142,145 @@ Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+/* ============ Global Variables & Player/Enemy Instances ================== */
+
+let points = document.querySelector('.points-value');
+let gameLevel = 1;
+
 // instantiate player and enemy objects
-const enemy1 = new Enemy(-90, 70);
-const enemy2 = new Enemy(-90, 153);
-const enemy3 = new Enemy(-90, 236);
-const allEnemies = [enemy1];
+const enemy1 = new Enemy(-90, 153, 'images/alien1.png', 65, 70, 'lr');
+const enemy2= new Enemy(-95, 70, 'images/alien4.png', 60, 50, 'lr');
+const enemy3 = new Enemy(-100, 236, 'images/alien2.png', 65,  70, 'lr');
+const enemy4 = new Enemy(510, 319, 'images/alien3.png', 60, 50, 'rl');
+const enemy5 = new Enemy(500, 100, 'images/tiefighter.png', 65, 70, 'rl');
+const enemy6 = new Enemy(-90, 259, 'images/tiefighter.png', 65, 70, 'lr');
+const enemy7 = new Enemy(510, 319, 'images/alien4.png', 60, 50, 'rl');
+
+let allEnemies = [enemy1];
 const player = new Player(200, 402);
 
+
+/* ================ Functions ================ */
+
+function updatePoints(){
+    // add 50 points each time the player makes it to space
+    points.innerHTML  = (points.innerHTML * 1) + 50;
+    if (gameLevel === 1){
+        levelOne()
+    }   
+}
+
+function levelOne(){
+    if (points.innerHTML == 50){
+        setTimeout(function(){
+            allEnemies.push(enemy2);
+        }, 800);
+    } else if (points.innerHTML == 100){
+        setTimeout(function(){
+            allEnemies.push(enemy3);
+        }, 800);
+    } 
+}
+
+function increaseLevelUi(){
+    let level = document.querySelector('.level-value');
+    /* set the level to the global gameLevel variable. NOTE: updateModal function increments this variable */
+    setTimeout(function(){
+        level.innerHTML = gameLevel;
+        points.innerHTML = 0;
+    }, 1500);
+    
+}
+
+function increaseDifficulty(){
+    setTimeout(function(){
+        if (gameLevel === 2){
+            allEnemies = [enemy1, enemy2, enemy3, enemy4];
+            allEnemies.forEach(function(enemy) {
+                enemy.render();        
+            });
+        }
+        else if (gameLevel === 3){
+            allEnemies = [enemy1, enemy2, enemy3, enemy7, enemy5, enemy6];
+            allEnemies.forEach(function(enemy) {
+                enemy.render();        
+            });
+        } 
+    }, 700);  
+}
+
+function winGame(){
+    // congratulate user for winning. ask if they want to play again
+    setTimeout(function(){
+        if (gameLevel === 4){
+            let winningModal = document.querySelector('.winning-modal');
+            winningModal.classList.add('show-winning-modal');
+
+            let replay = document.querySelector('.replay');
+            let quit = document.querySelector('.quit');
+
+            replay.addEventListener('click', replayGame);
+            quit.addEventListener('click', quitGame);
+        }
+    }, 1500);
+}
+
+function quitGame(){
+    document.querySelector('.congratulations-text').style.display = 'none';
+}
+
+function replayGame(){
+    // hide the modal. restart the game at level 1
+    setTimeout(function(){
+        winningModal.classList.toggle('show-winning-modal');
+    }, 1000);
+
+    setTimeout(() => window.location.reload(), 1000);
+}
+
+function updateModal(){
+    // increment the global gameLevel variable, and set the level in the modal to that value
+    gameLevel += 1;
+    let modalLevel = document.querySelector('.modal-level-value');
+    // only increment up to level 3 because player wins the game if they pass level 3
+    if (gameLevel <= 3){
+        modalLevel.innerHTML = gameLevel;
+    }
+}
+
+function winLevel(){ 
+    // update the level in the modal before showing the modal  
+    updateModal();
+
+    // if the game level is 4, the player has won the game
+    if (gameLevel === 4){
+        winGame();
+    } else {
+        const congratulations = document.querySelector('.congratulations');
+        player.x = player.x;
+    
+       // show the modal. hide the enemies
+        setTimeout(function(){
+            allEnemies.length = 0;
+            congratulations.classList.toggle('show-congratulations');
+        }, 1000);
+
+        // while the modal is hiding the score panel, update those score pane UI values
+        increaseLevelUi();
+
+        // move player to starting position. inrease game difficulty
+        setTimeout(function(){
+            congratulations.classList.toggle('show-congratulations');
+            player.x = 200;
+            player.y = 402;  
+            increaseDifficulty();
+        }, 3000);
+    }
+    
+}
+
+
+/* ======================= Event Listeners ===================== */
 
 // This listens for key presses and sends the keys to the
 // Player.handleInput() method. 
