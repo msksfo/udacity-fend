66172f59c954b7b30059d35f53d38ff1e2313479
Tia Esguerra <msksfo@gmail.com>
2018-06-01 16:13:30 -0700

    Move all image source attributes out of html options element, into the cirrus object in js file

--- projects/catClicker/version1/scripts/main.js
+++ projects/catClicker/version1/scripts/main.js
@@ -13,19 +13,19 @@ let index = 0;
 
 // an array of objects. each object holds photo title, alt text, and number of likes
 const cirrus = [
-    {title: 'Sleepy Kitten', alt: 'Cirrus when she was a kitten, sleeping', hearts: 0},
-    {title: 'California Cat', alt: 'Cirrus leaning against the frame of a convertible with its top down', hearts: 0},
-    {title: 'Avgeek Cat', alt: 'Cirrus on the top of a ladder in front of a BT-13', hearts: 0},
-    {title: 'Birthday Cat', alt: 'Cirrus on her birthday, with a lit candle in her food', hearts: 0},
-    {title: 'Santa Cat', alt: 'Cirrus wearing her santa hat', hearts: 0},
-    {title: 'Pilot Cat', alt: 'Cirrus leaning out the window of a Cessna 172 before flying', hearts: 0},
-    {title: 'Sunbathing Cat', alt: 'Cirrus sitting on the bench on the balcony', hearts: 0},
-    {title: 'I Hate Mondays Cat', alt: 'Cirrus as a tiny kitten, exhausted, sleeping with feet splayed out in front of her', hearts: 0},
-    {title: 'Curious Passenger Cat', alt: 'Cirrus looking out the window while flying in a Cessna', hearts: 0},
-    {title: 'Thieving Cat', alt: 'Cirrus as a kitten, runnning away with a five dollar bill in her mouth', hearts: 0},
-    {title: 'Copilot Cat', alt: 'Cirrus watching daddy handle the controls of the Cessna', hearts: 0},
-    {title: 'Sunbathing Kitten', alt: 'Cirrus as a kitten sunbathing on the bench outside', hearts: 0},
-    {title: 'Family Portrait', alt: "Cirrus' first photo with her new family", hearts: 0},
+    {title: 'Sleepy Kitten', src: "images/sleepyCirrus.jpg", alt: 'Cirrus when she was a kitten, sleeping', hearts: 0},
+    {title: 'California Cat', src: "images/californiaCat.jpg", alt: 'Cirrus leaning against the frame of a convertible with its top down', hearts: 0},
+    {title: 'Avgeek Cat', src: "images/btCirrus.jpg", alt: 'Cirrus on the top of a ladder in front of a BT-13', hearts: 0},
+    {title: 'Birthday Cat', src: "images/happyBirthday.jpg", alt: 'Cirrus on her birthday, with a lit candle in her food', hearts: 0},
+    {title: 'Santa Cat', src: "images/santaCat.jpg", alt: 'Cirrus wearing her santa hat', hearts: 0},
+    {title: 'Pilot Cat', src: "images/clearProp.jpg", alt: 'Cirrus leaning out the window of a Cessna 172 before flying', hearts: 0},
+    {title: 'Sunbathing Cat', src: "images/sunbathing2.jpg", alt: 'Cirrus sitting on the bench on the balcony', hearts: 0},
+    {title: 'I Hate Mondays Cat', src: "images/imtoocute.jpg", alt: 'Cirrus as a tiny kitten, exhausted, sleeping with feet splayed out in front of her', hearts: 0},
+    {title: 'Curious Passenger Cat', src: "images/windowSeat.jpg", alt: 'Cirrus looking out the window while flying in a Cessna', hearts: 0},
+    {title: 'Thieving Cat', src: "images/thievingCat.jpg", alt: 'Cirrus as a kitten, runnning away with a five dollar bill in her mouth', hearts: 0},
+    {title: 'Copilot Cat', src: "images/copilot.jpg", alt: 'Cirrus watching daddy handle the controls of the Cessna', hearts: 0},
+    {title: 'Sunbathing Kitten', src: "images/sunbathing1.jpg", alt: 'Cirrus as a kitten sunbathing on the bench outside', hearts: 0},
+    {title: 'Family Portrait', src: "images/familyPortrait.jpg", alt: "Cirrus' first photo with her new family", hearts: 0},
 ];
 
 
@@ -38,7 +38,7 @@ select.addEventListener('change', function(e){
     index = e.target.options.selectedIndex;
 
     // get the image source of the option which was chosen 
-    let chosenImageSource = options[index].value;
+    let chosenImageSource = cirrus[index].src
 
     // set the src of the img to be the src of the option that was chosen 
     image.src = chosenImageSource;
