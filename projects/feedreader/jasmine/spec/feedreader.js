a1a296532b2d1ad969808a37b16096529a54b6b0
Tia Esguerra <msksfo@gmail.com>
2018-06-15 17:26:12 -0700

    Clean up comments and spacing

--- projects/feedreader/jasmine/spec/feedreader.js
+++ projects/feedreader/jasmine/spec/feedreader.js
@@ -1,27 +1,15 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+/* All of the tests are within the $() function, to ensure the DOM is ready 
+ * before the tests are run
  */
 $(function() {
 
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -53,11 +41,8 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function(){
-
-        // test ensures the menu element is hidden by default. 
-        
+        // test ensures the menu element is hidden by default.        
         let icon = document.querySelector('.menu-icon-link');
         let body = document.querySelector('body');
         
@@ -78,9 +63,6 @@ $(function() {
     });
         
 
-        
-
-    /* TODO: Write a new test suite named "Initial Entries" */  
     describe('Initial Entries', function(){
         /* test ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -103,7 +85,6 @@ $(function() {
     });
         
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function(){
         /* test ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
