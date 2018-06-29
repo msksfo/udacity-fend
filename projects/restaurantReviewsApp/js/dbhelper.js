6e64e861617f7f3d7deace413f7ad06f06f7e00b
Tia Esguerra <msksfo@gmail.com>
2018-06-28 18:59:35 -0700

    Update file paths in dbhelper file

--- projects/restaurantReviewsApp/js/dbhelper.js
+++ projects/restaurantReviewsApp/js/dbhelper.js
@@ -11,7 +11,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    //return `http://localhost:${port}/data/restaurants.json`;
+    return 'https://msksfo.github.io/udacity-fend/projects/restaurantReviewsApp/data/restaurants.json';
   }
 
   /**
@@ -152,7 +153,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   // Restaurant image alt text
